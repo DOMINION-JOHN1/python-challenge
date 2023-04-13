791d67480c64f6d8da3f404325783e8d55c99a39
John Dominion <jdominion123@gmail.com>
2023-04-13 20:21:46 +0100

    renaming and change of file format

---
+++ CHALLENGE 1.1/comparison statement.py
@@ -0,0 +1,16 @@
+num1=7
+num2=12
+print(num2>num1)
+print(num1>num2)
+print(num2>=num1)
+print(num1>=num2)
+
+
+val1= 4*5-6
+val2= (2+1)*5
+
+print(val2)
+print(val1)
+
+print(val1<val2)
+print(val2<val1)
---
+++ CHALLENGE 1.1/dictionaries.py
@@ -0,0 +1,8 @@
+shop={'apple':40,'mango':20,'orange':43,'pawpaw':18,'pineapple':15,'onion':9,'banana':51,'pear':38}
+print(shop['mango'])
+
+shop['pear']=6
+print(shop)
+
+a=shop['apple'] + shop['banana']
+print(a)
