d1ac359063640c9b7a715cb38a53aab0ac3e02c0
John Dominion <jdominion123@gmail.com>
2023-04-26 00:07:44 +0100

    set the def function to receive multiple number in the array

--- CHALLENGE 2.0/def,multiple_3_calc.py
+++ CHALLENGE 2.0/def,multiple_3_calc.py
@@ -1,7 +1,7 @@
 def multi_calc(*array):
     num=float(input("enter value here; "))
     for i in array:
-         print((i%num)==0)
+         if i%num==0:
+             print(i)
 
-
-multi_calc(7,6,5,4,3,6,7,8,7,67,65,43,89)
+multi_calc(7,8,9,0,888,90,87,67,6,67,889,88,987)
