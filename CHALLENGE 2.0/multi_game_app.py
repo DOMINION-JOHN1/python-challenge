527ff4c59b4eac656b66a72bb4931b8dda990cc9
John Dominion <jdominion123@gmail.com>
2023-05-08 10:54:43 +0100

    add function that display various comments after each answer.

--- CHALLENGE 2.0/multi_game_app.py
+++ CHALLENGE 2.0/multi_game_app.py
@@ -17,15 +17,19 @@ def repeat():
 
 
 def multi_tutor():
+    good_response = {1: "Very good!", 2: "Excellent!", 3: "Nice work!", 4: "Keep up the good work!"}
+    bad_response = {1: "No. Please try again.", 2: "Wrong. Try once more.", 3: "Don't give up!", 4: "No. Keep trying."}
+    good=good_response[randrange(1,4)]
+    poor=bad_response[randrange(1,4)]
     x, y = randrange(1, 10), randrange(1, 10)
     print(f'how much is {x} time {y}?')
     answer = x * y
     student = int(input("what is the answer "))
     if answer == student:
-        print('very good')
+        print(good)
         repeat()
     else:
-        print('No,please try again')
+        print(poor)
 
 
 multi_tutor()
