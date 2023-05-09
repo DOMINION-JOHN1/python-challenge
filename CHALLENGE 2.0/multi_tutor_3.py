f8f3cabdfa6a360ee76887d01483a5700b75c433
John Dominion <jdominion123@gmail.com>
2023-05-09 22:36:21 +0100

    add break to end the loop when the coumt reaches 10

--- CHALLENGE 2.0/multi_tutor_3.py
+++ CHALLENGE 2.0/multi_tutor_3.py
@@ -23,29 +23,25 @@ def multi_tutor():
     poor=bad_response[randrange(1,4)]
     correct_count = 0
     incorrect_count = 0
+
     while True:
         x, y = randrange(1, 10), randrange(1, 10)
-        while True:
-            print(f'how much is {x} time {y}?')
-            answer = x * y
-            student = int(input("what is the answer "))
-            if answer == student:
-                print(poor)
-                incorrect_count += 1
+        print(f'how much is {x} time {y}?')
+        answer = x * y
+        student = int(input("what is the answer "))
+        if answer != student:
+            print(poor)
+            incorrect_count += 1
+        else:
+            print(good)
+            correct_count += 1
+            if correct_count + incorrect_count == 10:
+                percentage = correct_count / (correct_count + incorrect_count)
+                if percentage < 0.75:
+                   print("Please ask your teacher for extra help.")
+                else:
+                    print("Congratulations, you are ready to go to the next level!")
                 break
-            else:
-                print(good)
-                incorrect_count += 1
-                if correct_count + incorrect_count == 10:
-                    percentage = correct_count / (correct_count + incorrect_count)
-                    if percentage < 0.75:
-                        print("Please ask your teacher for extra help.")
-                        break
-                    else:
-                        print("Congratulations, you are ready to go to the next level!")
-                    break
-
-
 
 multi_tutor()
 
