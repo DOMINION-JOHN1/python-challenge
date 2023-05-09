84866a31b869d0476376e6b80b2d822bd66be707
John Dominion <jdominion123@gmail.com>
2023-05-09 13:31:55 +0100

    trying a lot of options

--- CHALLENGE 2.0/multi_tutor_3.py
+++ CHALLENGE 2.0/multi_tutor_3.py
@@ -21,15 +21,32 @@ def multi_tutor():
     bad_response = {1: "No. Please try again.", 2: "Wrong. Try once more.", 3: "Don't give up!", 4: "No. Keep trying."}
     good=good_response[randrange(1,4)]
     poor=bad_response[randrange(1,4)]
-    x, y = randrange(1, 10), randrange(1, 10)
-    print(f'how much is {x} time {y}?')
-    answer = x * y
-    student = int(input("what is the answer "))
-    if answer == student:
-        print(good)
-        repeat()
-    else:
-        print(poor)
+    correct_count = 0
+    incorrect_count = 0
+    while True:
+        x, y = randrange(1, 10), randrange(1, 10)
+        while True:
+            print(f'how much is {x} time {y}?')
+            answer = x * y
+            student = int(input("what is the answer "))
+            if answer == student:
+                print(poor)
+                incorrect_count += 1
+                break
+            else:
+                print(good)
+                incorrect_count += 1
+                if correct_count + incorrect_count == 10:
+                    percentage = correct_count / (correct_count + incorrect_count)
+                    if percentage < 0.75:
+                        print("Please ask your teacher for extra help.")
+                        break
+                    else:
+                        print("Congratulations, you are ready to go to the next level!")
+                    break
+
 
 
 multi_tutor()
+
+
