7cd85bcd9abd34341fed1bd53244f6b0a21d8fe8
John Dominion <jdominion123@gmail.com>
2023-05-16 14:09:09 +0100

    conversion of the answer to strings so it can allow empty and strings input of the user removal of break function

--- CHALLENGE 2.0/multplication_tutor_master.py
+++ CHALLENGE 2.0/multplication_tutor_master.py
@@ -2,23 +2,24 @@ import random
 good=random.choice(["Very good!", "Excellent!", "Nice work!", "Keep up the good work!"])
 fail=random.choice(["No. Please try again.", "Wrong. Try once more.", "Don't give up!", "No. Keep trying."])
 def generate_question():
-    num1 = random.randint(10, 999)
-    num2 = random.randint(10, 999)
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
     question = f"How much is {num1} times {num2}? "
     answer = num1 * num2
-    return question, answer
+    correct_answer=answer.__str__()
+    return question, correct_answer
 
 def main_game():
     num_correct = 0
     num_incorrect = 0
     print("Welcome to the multiplication game!")
-    question, answer = generate_question()
+    question, correct_answer = generate_question()
     while True:
         user_input = int(input(question))
-        if user_input == answer:
+        if user_input == correct_answer:
             print(good)
             num_correct+=1
-            question, answer = generate_question()
+            question, correct_answer = generate_question()
         else:
             print(fail)
             num_incorrect+=1
@@ -28,6 +29,6 @@ def main_game():
                     print("Please ask your teacher for extra help.")
                 else:
                     print("Congratulations, you are ready to go to the next level!")
-                    break
-                break
+
+
 main_game()
