a81fe8cd981b8010527d6bdcff3df31e5e12790f
John Dominion <jdominion123@gmail.com>
2023-05-16 13:53:07 +0100

    conversion of the answer to strings so it can allow empty and strings input of the user

--- CHALLENGE 2.0/maths_multiplication_tutor_level1.py
+++ CHALLENGE 2.0/maths_multiplication_tutor_level1.py
@@ -5,16 +5,17 @@ def generate_question():
     num2 = random.randint(0, 9)
     question = f"How much is {num1} times {num2}? "
     answer = num1 * num2
-    return question, answer
+    correct_answer=answer.__str__()
+    return question, correct_answer
 
 def main_game():
     print("Welcome to the multiplication game!")
-    question, answer = generate_question()
+    question, correct_answer = generate_question()
     while True:
-        user_input = int(input(question))
-        if user_input == answer:
+        user_input = str(input(question))
+        if user_input == correct_answer:
             print("Very good!")
-            question, answer = generate_question()
+            question, correct_answer = generate_question()
         else:
             print("No. Please try again.")
 
