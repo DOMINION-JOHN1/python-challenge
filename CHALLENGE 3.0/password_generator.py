93dc5ad8f2fb58afad97802e401ec8bd4b2db6d7
John Dominion <jdominion123@gmail.com>
2023-06-10 22:06:51 +0100

    generate characters for the passwords

---
+++ CHALLENGE 3.0/password_generator.py
@@ -0,0 +1,8 @@
+import random
+import string
+# Generating characters for the passwords
+uppercase_letters = string.ascii_uppercase
+lowercase_letters = string.ascii_lowercase
+special_characters = string.punctuation
+numbers = string.digits
+
