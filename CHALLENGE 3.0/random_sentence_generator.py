3b68a34c2a5f13371f0ee612732408297a2d41f7
John Dominion <jdominion123@gmail.com>
2023-07-04 21:30:08 +0100

    add code to randomly choose from articles, nouns,verbs and prepositions. addition of range in a FOR loop to make sure 20 sentences are generated

---
+++ CHALLENGE 3.0/random_sentence_generator.py
@@ -0,0 +1,15 @@
+import random
+
+articles = ["the", "a", "one", "some", "any"]
+nouns = ["boy", "girl", "dog", "town", "car"]
+verbs = ["drove", "jumped", "ran", "walked", "skipped"]
+prepositions = ["to", "from", "over", "under", "on"]
+
+
+def generate_sentence():
+    sentence = [random.choice(articles), random.choice(nouns), random.choice(verbs), random.choice(prepositions), random.choice(articles), random.choice(nouns)]
+    return " ".join(sentence).capitalize() + "."
+
+
+for _ in range(20):
+    print(generate_sentence())
