4333d80886bdb73f917247896acb65827bffc8c4
John Dominion <jdominion123@gmail.com>
2023-07-05 12:39:34 +0100

    dit of the function to make it a 12 hours clock and use of numbers for the hour mark instead of words

--- CHALLENGE 3.0/time_in_words.py
+++ CHALLENGE 3.0/time_in_words.py
@@ -1,3 +1,21 @@
-
 def time_in_words(hour, minute):
-    numbers = range(29)
+
+    if minute == 0:
+        return f"{hour % 12} o'clock"
+    elif minute == 1:
+        return f"one minute past {hour}"
+    elif minute == 15:
+        return f"quarter past {hour}"
+    elif minute == 30:
+        return f"half past {hour}"
+    elif minute == 45:
+        return f"quarter to {(hour+1) % 12}"
+    elif minute == 59:
+        return f"one minute to {(hour+1) % 12}"
+    elif minute < 30:
+        return f"{minute} minutes past {hour}"
+    else:
+        return f"{60-minute} minutes to {(hour+1) % 12}"
+
+
+print(time_in_words(13, 0))
